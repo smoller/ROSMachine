814ba78fc2a020f029409ffe29b5e89a94a4610b
Simon Moller <Simon@Simons-MacBook-Air.local>
2014-08-30 11:43:44 +1000

    Initial Commit

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
+wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
+
+sudo aptitude update -y
+sudo aptitude install -y build-essential git vim 
+sudo aptitude install -y python libpq-dev python-dev python-setuptools python-virtualenv
+sudo aptitude install -y ros-indigo-desktop-full ubuntu-desktop
+sudo rosdep init
+rosdep update -y
+echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo aptitude install -y python-rosinstall
